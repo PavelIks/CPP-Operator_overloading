1c59015478b1755d45bf4ed2791199d988605a5c
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-06-15 19:39:14 +0300

    Updated-2.

--- CPPSixthAPP.cpp
+++ CPPSixthAPP.cpp
@@ -3,55 +3,62 @@
 
 #include <iostream>
 
-class Drod
+class Drob
 {
 public:
-	Drod()
+	Drob()
 	{
 		this->znamenatel1 = this->chislitel1 = 0;
 	}
-	Drod(int chislitel1, int znamenatel1)
+	Drob(int chislitel1, int znamenatel1)
 	{
-		this->chislitel1 = chislitel1;
-		this->znamenatel1 = znamenatel1;
+		this->chislitel1 = new int(chislitel1);
+		this->znamenatel1 = new int(znamenatel1);
 	}
 
 	// static
-	/*Drod operator +(Drod& other)
+	/*Drob operator +(Drob& other)
 	{
-		return Drod
+		return Drob
 		(
 			other.get_chislitel1() + this->get_chislitel1(),
 			other.get_znamenatel1() + this->get_znamenatel1()
 		);
 	}*/
 	// dynamic
-	inline Drod* operator +(Drod& other)
+	inline Drob* operator +(Drob& other)
 	{
-		return new Drod
-		(
-			//other.get_chislitel1() + this->get_chislitel1(),
-			//other.get_znamenatel1() + this->get_znamenatel1()
+		if (other.get_chislitel1() == other.get_znamenatel1())
+		{
+			return new Drob
+			(
+				other.get_chislitel1() /*+ this->get_chislitel1()*/, other.get_znamenatel1()
+				//other.get_chislitel1() + this->get_chislitel1(),
+					//other.get_znamenatel1() + this->get_znamenatel1()
 
-			this->get_znamenatel1() + this->get_chislitel1(),
-			other.get_chislitel1() + other.get_znamenatel1()
-		);
+			);
+		}
 	}
 	int get_chislitel1()
 	{
-		return this->chislitel1;
+		return *this->chislitel1;
 	}
 	int get_znamenatel1()
 	{
-		return this->znamenatel1;
+		return *this->znamenatel1;
 	}
 	void show()
 	{
-		std::cout << this->get_chislitel1() << " : " << this->get_znamenatel1() << "\n";
+		std::cout << this->get_chislitel1() << "\n" << "---" << "\n" << this->get_znamenatel1() << "\n";
+	}
+
+	~Drob()
+	{
+
 	}
 
 private:
-	int chislitel1, znamenatel1;
+	int* chislitel1, * znamenatel1;
 };
 
 int main()
@@ -60,16 +67,17 @@ int main()
 	int znamenatel1 = 8;
 	int c = chislitel1 + znamenatel1;
 	std::cout << chislitel1 + znamenatel1;*/
-	Drod test_1(3, 2);
-	Drod test_2(5, 4);
+	Drob drob1(3, 2);
+	Drob drob2(5, 4);
 
+	drob1.show();
 	// static
-	//Drod test_res = test_1 + test_2;
+	//Drob test_res = drob1 + drob2;
 	//test_res.show();
 
 	// dynamic
-	Drod* test_res = test_1 + test_2;
-	test_res->show();
+	Drob* test_res = drob1 + drob2;
+	//test_res->show();
 
 	// error
 	//std::cout << *test_res;
