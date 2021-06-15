b184d99cadb3c55e48349bdfa8ad48d35c6cc3d9
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-06-15 20:32:12 +0300

    Updated-5.

--- CPPSixthAPP.cpp
+++ CPPSixthAPP.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+﻿/*Динамика*/
+#include <iostream>
 
 class C_Test
 {
@@ -10,43 +11,53 @@ public:
 	}
 	C_Test(int a, int b)
 	{
-		this->a = new int(a);
-		this->b = new int(b);
+		/*this->a = new int(a);
+		this->b = new int(b);*/
+		
+		this->a = a;
+		this->b = b;
 	}
-	inline C_Test* operator +(C_Test& other)
+	C_Test operator +(C_Test& other)
+	{
+		return C_Test(other.getA() + this->getA(), other.getB() + this->getB());
+	}
+	/*inline C_Test* operator +(C_Test& other)
 	{
 		return new C_Test(*other.getA() + *this->getA(), *other.getB() + *this->getB());
-	}
-	int* getA()
+	}*/
+	/*int* getA()
 	{
 		return this->a;
 	}
 	int* getB()
 	{
 		return this->b;
+	}*/
+	int getA()
+	{
+		return this->a;
+	}
+	int getB()
+	{
+		return this->b;
 	}
 	void show()
 	{
-		std::cout << *this->getA() << " : " << *this->getB() << "\n";
+		/*std::cout << *this->getA() << " : " << *this->getB() << "\n";*/
+		std::cout << this->getA() << " : " << this->getB() << "\n";
 	}
 private:
-	int* a;
-	int* b;
+	/*int* a;
+	int* b;*/
+	int a;
+	int b;
 };
 
 int main()
 {
-	/*int a = 7;
-	int b = 8;
-	int c = a + b;
-	std::cout << a + b;*/
-
 	C_Test test_1(9, 9);
 	C_Test test_2(7, 7);
-
-	C_Test* test_res = test_1 + test_2;
-	test_res->show();
-
-	//		error
-	//std::cout << *test_res;
+	C_Test test_res = test_1 + test_2;
+	test_res.show();
+	/*test_res->show();*/
 }
