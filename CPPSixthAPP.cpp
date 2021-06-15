0615a7202b09653107bb2ed51ebfd2fb8f31068e
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-06-15 20:08:09 +0300

    Updated-4.

--- CPPSixthAPP.cpp
+++ CPPSixthAPP.cpp
@@ -5,37 +5,33 @@ class C_Test
 public:
 	C_Test()
 	{
-		this->b = this->a = 0;
+		this->a = 0;
+		this->b = 0;
 	}
 	C_Test(int a, int b)
 	{
-		this->a = a;
-		this->b = b;
+		this->a = new int(a);
+		this->b = new int(b);
 	}
-	//		1
-	/*C_Test operator +(C_Test& other)
-	{
-		return C_Test(other.getA() + this->getA(), other.getB() + this->getB());
-	}*/
-	//		2
 	inline C_Test* operator +(C_Test& other)
 	{
-		return new C_Test(other.getA() + this->getA(), other.getB() + this->getB());
+		return new C_Test(*other.getA() + *this->getA(), *other.getB() + *this->getB());
 	}
-	int getA()
+	int* getA()
 	{
 		return this->a;
 	}
-	int getB()
+	int* getB()
 	{
 		return this->b;
 	}
 	void show()
 	{
-		std::cout << this->getA() << " : " << this->getB() << "\n";
+		std::cout << *this->getA() << " : " << *this->getB() << "\n";
 	}
 private:
-	int a, b;
+	int* a;
+	int* b;
 };
 
 int main()
@@ -48,11 +44,6 @@ int main()
 	C_Test test_1(9, 9);
 	C_Test test_2(7, 7);
 
-	//		1
-	//C_Test test_res = test_1 + test_2;
-	//test_res.show();
-
-	//		2
 	C_Test* test_res = test_1 + test_2;
 	test_res->show();
 
